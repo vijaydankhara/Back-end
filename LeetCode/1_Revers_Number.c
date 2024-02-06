f264f7ed4ea706df37eb1c2a1ca10b58b8e78082
vijay dankhara <vijaydankhara111@gmail.com>
2024-02-06 08:47:09 +0530

    06-02-2024

--- LeetCode/1_Revers_Number.c
+++ LeetCode/1_Revers_Number.c
@@ -2,17 +2,14 @@
 
 int main()
 {
-    int number;
-    // int number = 123;
+    int number = 123;
     int x = 0;
-    
-    printf("Enter a number: ");
-    scanf("%d", &number);
     
     while(number != 0)
     {
-        x = x * 10 + number % 10;
-        number = number / 10;
+        x = x * 10 + number % 10;   //  0 * 10 + 3 = 3   3 * 10 + 2 = 32    32 * 10 + 1 = 321
+
+        number = number / 10;     // 3 / 10 = 0  32 / 10 = 3  321 / 10 = 32
     }
     
     printf("Reversed Number: %d", x);
