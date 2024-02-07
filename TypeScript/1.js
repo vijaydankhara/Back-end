d034f6497b225808cca20395f7f5b4cfdc673205
vijay dankhara <vijaydankhara111@gmail.com>
2024-02-07 09:43:12 +0530

    07-02-2024

--- TypeScript/1.js
+++ TypeScript/1.js
@@ -1,21 +1,33 @@
 // user difined type
 // array
+
 // var a: number = 5;
 // console.log(a);
+
 // var b: string= "hello";
 // console.log(b);
+
 // var c : number[]= [11,22,33,44,55];
 // console.log(c);
+
 // var d : string[]= ["hello","surat","how","are","you"];
 // console.log(d);
+
 //////////////////////////////////////////////////////////////
+
+
 // touple
+
 // let a : [string,number];
 // a = ["hello",10];
 // console.log(a);
+
 // let x : number | string = 11;
 // console.log(x);
+
 ///////////////////////////////
+
+
 // function find()
 // {
 //     let x = -10 ;
@@ -28,14 +40,17 @@
 //     }
 // }
 // find();
+
+
 //////////////////////////////
-function votting() {
-    var x = 16;
-    if (x >= 18) {
-        console.log(" Elegebal for votting");
-    }
-    else {
-        console.log(" Elegebal for  not votting");
-    }
-}
-votting();
+
+// function votting() {
+//     var x = 16;
+//     if (x >= 18) {
+//         console.log(" Elegebal for votting");
+//     }
+//     else {
+//         console.log(" Elegebal for  not votting");
+//     }
+// }
+// votting();
