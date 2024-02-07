0b26dee19c78bbbc51b44e4de796ea3774af11b3
vijay dankhara <vijaydankhara111@gmail.com>
2024-02-07 09:37:36 +0530

    07-02-2024

--- TypeScript/1.js
+++ TypeScript/1.js
@@ -1,4 +1,41 @@
-var b = 50;
-var c = 2;
-var d = b * c;
-console.log(d);
+// user difined type
+// array
+// var a: number = 5;
+// console.log(a);
+// var b: string= "hello";
+// console.log(b);
+// var c : number[]= [11,22,33,44,55];
+// console.log(c);
+// var d : string[]= ["hello","surat","how","are","you"];
+// console.log(d);
+//////////////////////////////////////////////////////////////
+// touple
+// let a : [string,number];
+// a = ["hello",10];
+// console.log(a);
+// let x : number | string = 11;
+// console.log(x);
+///////////////////////////////
+// function find()
+// {
+//     let x = -10 ;
+//     if(x>=0)
+//     {
+//         console.log(" this is positive number");
+//     }else
+//     {
+//         console.log(" this is Negative number");
+//     }
+// }
+// find();
+//////////////////////////////
+function votting() {
+    var x = 16;
+    if (x >= 18) {
+        console.log(" Elegebal for votting");
+    }
+    else {
+        console.log(" Elegebal for  not votting");
+    }
+}
+votting();
--- TypeScript/1.ts
+++ TypeScript/1.ts
@@ -1,5 +1,67 @@
 
-let b = 50
-let c = 2
-let d = b * c
-console.log(d);
+// user difined type
+// array
+
+// var a: number = 5;
+// console.log(a);
+
+// var b: string= "hello";
+// console.log(b);
+
+// var c : number[]= [11,22,33,44,55];
+// console.log(c);
+
+// var d : string[]= ["hello","surat","how","are","you"];
+// console.log(d);
+
+//////////////////////////////////////////////////////////////
+
+// touple
+ 
+// let a : [string,number];
+// a = ["hello",10];
+// console.log(a);
+
+// let x : number | string = 11;
+// console.log(x);
+
+///////////////////////////////
+
+// function find()
+// {
+//     let x = -10 ;
+//     if(x>=0)
+//     {
+//         console.log(" this is positive number");
+        
+//     }else
+//     {
+//         console.log(" this is Negative number");
+//     }
+
+// }
+// find();
+
+//////////////////////////////
+
+
+// function votting()
+// {
+//     let x = 16 ;
+//     if(x>=18)
+//     {
+//         console.log(" Elegebal for votting");
+        
+//     }else
+//     {
+//         console.log("Not Elegebal for votting");
+//     }
+
+// }
+// votting();
+
+
+
+
+
+
