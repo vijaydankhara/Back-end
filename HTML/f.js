7d97f2467382297bb66482b75631a1254e6a897d
vijay dankhara <vijaydankhara111@gmail.com>
2024-02-12 07:54:24 +0530

    12-02-2024

---
+++ HTML/f.js
@@ -0,0 +1,15 @@
+let a=document.getElementById("img");
+function myfunon()
+{a.style.animationDuration = 3+"s";}
+function myfunoff()
+{a.style.animationDuration = 0+"s";}
+function myfun1()
+{a.style.animationDuration = 1+"s";}
+function myfun2()
+{a.style.animationDuration = 0.9+"s";}
+function myfun3()
+{a.style.animationDuration = 0.6+"s";}
+function myfun4()
+{a.style.animationDuration = 0.3+"s";}
+function myfun5()
+{a.style.animationDuration = 0.1+"s";}
