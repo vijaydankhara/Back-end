36bb667338c47834f80fecce78b91307047f7a4f
vijay dankhara <vijaydankhara111@gmail.com>
2024-02-15 09:48:17 +0530

    15-02-2024

---
+++ Node_js/basic.js
@@ -0,0 +1,7 @@
+// add numbers
+const add = (x,y) => x + y;
+ 
+// product number
+const product =(x,y)=> x * y;
+
+module.exports = {add, product}
--- Node_js/fs.js
+++ Node_js/fs.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
-
+const { add,product}=require("./basic.js");
+const { log } = require("console");
 // fs.open('abc.txt',(err, data)=>{
 //     if(err){
 //         console.log('failed !!! file is not open ');
@@ -9,5 +10,24 @@ const fs = require("fs");
 //     }
 // })
 
-fs.openSync('abc.txt');
-console.log('file open sucessfully');
+// fs.openSync('abc.txt');
+// console.log('file open sucessfully');
+
+//////////////////////////////////////////
+// read method 
+
+// fs.readFile('abc.txt','utf8',(err,data)=>{
+//     if(err)
+//     console.log("Failde to read file");
+// else
+// console.log(data);
+// });
+
+
+// let data = fs.readFileSync('abc.txt','utf8');
+// console.log(data);
+
+
+///////////////////////////////////
+//write mode
+
