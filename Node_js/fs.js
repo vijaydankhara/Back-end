add6856c75370a62eab46c77206c1f7a9fb492eb
vijay dankhara <vijaydankhara111@gmail.com>
2024-02-15 19:34:00 +0530

    15-02-2024

--- Node_js/fs.js
+++ Node_js/fs.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 const { add,product}=require("./basic.js");
 const { log } = require("console");
+
+/* ======= File Open Method ======= */
+// ==>Asynchronously Method
 // fs.open('abc.txt',(err, data)=>{
 //     if(err){
 //         console.log('failed !!! file is not open ');
@@ -10,12 +13,14 @@ const { log } = require("console");
 //     }
 // })
 
+// ==>synchronously Method
 // fs.openSync('abc.txt');
 // console.log('file open sucessfully');
 
-//////////////////////////////////////////
-// read method 
 
+/* ======= File Read Method ======= */
+
+// ==>Asynchronously Method
 // fs.readFile('abc.txt','utf8',(err,data)=>{
 //     if(err)
 //     console.log("Failde to read file");
@@ -23,11 +28,70 @@ const { log } = require("console");
 // console.log(data);
 // });
 
-
+// ==>synchronously Method
 // let data = fs.readFileSync('abc.txt','utf8');
 // console.log(data);
 
 
-///////////////////////////////////
-//write mode
+/* ======= File Write Mode ======= */
 
+// ==> Asynchronously Method
+// let txt =`#include<stdio.h>
+// int main(){
+//     printf("Welcome In The World");
+// }`;
+
+// fs.writeFile("basic.c",txt,(err,data)=>{
+//     if(err) console.log("Failed To Write File");
+//     else console.log("File Written sucessfuly");
+// }
+// );
+
+// ==> synchronously Method
+// fs.writeFileSync("hello.c",txt);
+// console.log("File Written Sucessfuly");
+
+
+/* ======= File Append Method ======= */
+
+// ==>Asynchronously Method
+// let txt = `// Good Morning`;
+// fs.appendFile("hello.c",txt,(err,data)=>{
+//     if (err) console.log("Failed To Append File");
+//     else console.log("Successfully Appended File");
+// });
+
+
+// ==>synchronously Method
+
+
+
+
+
+
+
+
+/* ======= File Rename Method ======= */
+
+// ==>Asynchronously Method
+// fs.rename('basic.c','hello2.c',(err)=>{
+//     if (err) console.log("Failed To Rename File");
+//     else console.log("Successfully Renamed File");
+// });
+
+// ==>synchronously Method
+
+
+
+
+
+
+/* ======= File Close Method ======= */
+// ==>Asynchronously Method
+
+// fs.close(2,(err)=>{
+//     if (err) console.log("Failed To Close File");
+//     else console.log("Successfully Closed File");
+// });
+
+// ==>Asynchronously Method
---
+++ Node_js/hello2.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+int main(){
+    printf("Welcome In The World");
+}
