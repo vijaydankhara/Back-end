b2bd420015747a55c0cce36c1666bbf02146c418
vijay dankhara <vijaydankhara111@gmail.com>
2024-02-16 09:39:27 +0530

    16-02-2024

---
+++ Node_js/os.js
@@ -0,0 +1,15 @@
+// os   => operating system
+const os = require('os');
+
+
+console.log(os.EOL);
+
+// console.log(os.arch());     // x64
+// console.log(os.availableParallelism());    // 4
+// console.log(os.cpus());
+// console.log(os.freemem());    // 11633786880
+// console.log(os.totalmem());      // 17054949376
+// console.log(os.networkInterfaces());
+// console.log(os.getPriority());       // 0
+// console.log(os.homedir());     // C:\Users\Dell
+// console.log(os.hostname());      // DESKTOP-1KO5PEA
