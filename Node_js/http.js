4d6a8d012b7e83e0bfc89f5e57caa4aca8496f39
vijay dankhara <vijaydankhara111@gmail.com>
2024-02-19 09:44:11 +0530

    19-02-2024

--- Node_js/http.js
+++ Node_js/http.js
@@ -1,8 +1,20 @@
 // http Module 
 const http = require('http');
+const fs = require("fs");
+// const data = fs.readFileSync('./hello.json','utf-8')
+const data = fs.readFileSync('./abc.txt','utf-8')
+
 const server = http.createServer((req, res) => {
-    res.end('Welcome To Lacal Server....');
+    // res.setHeader('Dummy','Dummy Value');
+    // res.setHeader('content-type','text/html');
+    // res.setHeader('content-type','application/json');
+    // res.end('Welcome To Lacal Server....');
+    res.write('<h1 style="color:red">What is JavaScript ?</h1>')
 }); // server creat
+
+server.on('request',(req,res)=>{
+    res.end(data)
+})
 
 server.listen(3193,()=>{
     console.log("Server Start At A Portr 3193");
